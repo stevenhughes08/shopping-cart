894c9f92217c78b9cc3e90617b41361d2ef858d5
Steven <stevenhughes08@gmail.com>
2019-07-08 20:27:22 -0400

    updated home.js linked to reducer with props

--- shopping-cart/src/components/Home.js
+++ shopping-cart/src/components/Home.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Home extends Component {
     render() {
+        let itemList = this.props.items.map(item => {
+            return (
+                <div className="card" key={item.id}>
+                    <div className="card-image">
+                        <img src={item.img} alt={item.title} />
+                        <span className="card-title">{item.title}</span>
+                        <span
+                            to="/"
+                            className="btn-floating halfway-fab waves-effect waves-light red"
+                        >
+                            <i className="material-icons">add</i>
+                        </span>
+                    </div>
+                </div>
+            );
+        });
         return (
             <div className="container">
-                <h3>Home</h3>
+                <h3 className="center">Our items</h3>
+                <div className="box">{itemList}</div>
             </div>
         );
     }
 }
 
-export default Home;
+const mapStateToProps = state => {
+    return {
+        items: state.items
+    };
+};
+export default connect(mapStateToProps)(Home);
