acb0afa11d06a95327888198774a35bd7452997c
Steven <stevenhughes08@gmail.com>
2019-07-08 20:28:07 -0400

    updated Navbar.js

--- shopping-cart/src/components/Navbar/Navbar.js
+++ shopping-cart/src/components/Navbar/Navbar.js
@@ -1,30 +1,29 @@
 import React from "react";
-import "../layout/CSS/custom.css";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Router, Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="nav-wrapper">
-            <Router>
-                <div className="container">
+            <div className="container">
+                <Router>
                     <Link to="/" className="brand-logo">
                         Shopping
                     </Link>
-
                     <ul className="right">
                         <li>
                             <Link to="/">Shop</Link>
                         </li>
                         <li>
-                            <Link to="/cart">My Cart</Link>
+                            <Link to="/cart">My cart</Link>
                         </li>
                         <li>
                             <Link to="/cart">
-                                <i className="material-icons">shopping_cart</i>
+                                <i className="material-icons">Shopping Cart</i>
                             </Link>
                         </li>
                     </ul>
-                </div>
-            </Router>
+                </Router>
+            </div>
         </nav>
     );
 };
